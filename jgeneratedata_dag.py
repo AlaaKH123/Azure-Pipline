34d766cdea86bfde8a0f9ce1b863efa72c9ef4bd
root <root@flaskpusher-deployment-78755c4844-t7d6d>
2024-05-24 12:59:08 +0000

    Added new DAG for job jgeneratedata

---
+++ jgeneratedata_dag.py
@@ -0,0 +1,31 @@
+
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+}
+
+dag = DAG(
+    'jgeneratedata_dag',
+    default_args=default_args,
+    description='DAG for job jgeneratedata',
+    schedule_interval=None,
+)
+
+start = DummyOperator(
+    task_id='start',
+    dag=dag,
+)
+
+end = DummyOperator(
+    task_id='end',
+    dag=dag,
+)
+
+start >> end
