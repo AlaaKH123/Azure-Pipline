f09c3584618f45f4264129f11787e64775e62890
root <root@flaskpusher-deployment-76ffcc8d7-x7f6g>
2024-05-25 13:39:53 +0000

    Added new DAG for job example_job

--- example_job_dag.py
+++ example_job_dag.py
@@ -2,11 +2,11 @@
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.utils.dates import days_ago
+from airflow.operators.bash_operator import BashOperator
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': days_ago(1),
     'email_on_failure': False,
     'email_on_retry': False,
 }
@@ -15,11 +15,19 @@ dag = DAG(
     'example_job_dag',
     default_args=default_args,
     description='DAG for job example_job',
-    schedule_interval=None,
+    schedule_interval='0 30 12 * * 1,3,5',
+    start_date=days_ago(1),
+    catchup=False,
 )
 
 start = DummyOperator(
     task_id='start',
+    dag=dag,
+)
+
+run_job = BashOperator(
+    task_id='run_job',
+    bash_command='docker run example_image',
     dag=dag,
 )
 
@@ -28,4 +36,4 @@ end = DummyOperator(
     dag=dag,
 )
 
-start >> end
+start >> run_job >> end
